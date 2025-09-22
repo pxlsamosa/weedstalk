a0f972ffd04ffc8f2df8b86475e9da8d07fce432
MrEktidd <jake.lizotte@hotmail.com>
2025-09-22 02:19:43 -0400

    Update PlantResource.gd
    updated comments

--- resources/PlantResource.gd
+++ resources/PlantResource.gd
@@ -5,5 +5,6 @@ class_name PlantResource
 
 @export var plant_id: String = "" ## plant's id for calling via code plant_name or plant_1 etc.
 @export var plant_name: String = "" ## plant's player facing name Plant Name or Plant 1 etc.
-@export var min_yield: int = 100
-@export var max_yield: int = 400
+@export var min_yield: int = 100 ## plant's minimum yield on harvest
+@export var max_yield: int = 400 ## plant's maximum yield on harvest  
+#We might move yield to a new StrainResource
